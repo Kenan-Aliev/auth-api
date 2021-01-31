349b4cadaff8ed62c3ebfc9ffd8e39c53312bbb4
Kenan2112 <Kenan199948@gmail.com>
2021-01-31 19:31:15 +0600

    made verification

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -6,7 +6,23 @@ const config = require('config')
 const {check , validationResult} = require('express-validator')
 const User = require('../models/userModel')
 const ToDo = require('../models/todosModel')
+const nodemailer = require('nodemailer')
 const authMiddleware = require('../middlewares/auth.middleware')
+
+const transporter = nodemailer.createTransport({
+        host: 'smtp.mail.ru',
+        port: 465,
+        secure: true, // true for 465, false for other ports
+        auth: {
+            user: 'mailertest48@mail.ru',
+            pass: 'Nodemailer'
+        }
+    },{
+        from:'Mailer Test <mailertest48@mail.ru>'
+    }
+);
+
+
 router.post('/registration',
     [
         check('email','Uncorrect email').isEmail(),
@@ -37,27 +53,52 @@ router.post('/registration',
                 }
                 return res.status(400).json(errors);
             } else {
-                const hashPassword = await bcrypt.hash(password,8)
-                const newUser = await new User({
-                    email,
-                    username,
-                    phone,
-                    password: hashPassword
-                }).save()
-                await new ToDo({
-                    title:'Example title',
-                    description:'Example description',
-                    user:newUser._id
-                }).save()
-                return res.json({message:'User was created'})
+                const token = jwt.sign({email,username,phone,password},config.get('secretKey'),{expiresIn:'1h'})
+                const message = {
+                    from:'Mailer Test <mailertest48@mail.ru>',
+                    to:email,
+                    subject:'Account activation link',
+                    html:`<h2>Пожалуйста, нажмите по ссылке ниже,чтобы подтвердить регистрацию на нашем сайте</h2>
+                          <a href="${config.get('client-url')}/verification/${token}">Нажмите</a>`
+
+                }
+                await transporter.sendMail(message, (err, info) => {
+                    if (err) {
+                        return res.send({message:'Error',err})
+                    }
+                    return res.json({message:'Проверьте ваш почтовый ящик,чтобы подтвердить регистрацию',info})
+                })
             }
-    })
+     })
     }
     catch(error){
     return res.json({message:'Server error',error})
     }
 })
 
+router.post('/activation',async (req,res)=>{
+    const {token} = req.body
+    if(token){
+        try{
+            const decoded = jwt.verify(token,config.get('secretKey'))
+            const {email,username,phone,password} = decoded
+            const hashPassword = await bcrypt.hash(password,8)
+            const newUser = await new User({email,username,phone,password:hashPassword}).save()
+            await new ToDo({
+                title:'Example title',
+                description:'Example description',
+                user:newUser._id
+            }).save()
+            return res.json({message:'Поздравляем! Вы успешно заррегистрировались на нашем сайте'})
+        }
+        catch(e){
+            return res.json({message:'Неверный токен'})
+        }
+    }
+    else{
+        return res.json({message:'Что то пошло не так'})
+    }
+})
 
 
 router.post('/login',async (req,res)=>{
